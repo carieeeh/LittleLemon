b55b67779f4b28517831f970785b750b3444dd2f
Carol Villadelgado <carolvilladelgado@gmail.com>
2023-08-08 23:24:35 +0800

    Update footer

--- components/LittleLemonFooter.js
+++ components/LittleLemonFooter.js
@@ -1,23 +1,25 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function LittleLemonFooter() {
   return (
-    <View
-      style={{
-        backgroundColor: "#F4CE14",
-        marginBottom: 10,
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 18,
-          color: "black",
-          textAlign: "center",
-        }}
-      >
-        All rights reserved by Little Lemon, 2022{" "}
+    <View style={footerStyles.container}>
+      <Text style={footerStyles.itemText}>
+        All rights reserved by Little Lemon, 2022
       </Text>
     </View>
   );
 }
+
+const footerStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "#F4CE14",
+    marginBottom: 10,
+  },
+
+  itemText: {
+    fontSize: 18,
+    color: "black",
+    textAlign: "center",
+  },
+});
