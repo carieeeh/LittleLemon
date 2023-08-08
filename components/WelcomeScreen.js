8b6b378092280331422ea1bf4648fb19ceeed2db
Carol Villadelgado <carolvilladelgado@gmail.com>
2023-08-08 23:26:41 +0800

    update welcome screen

--- components/WelcomeScreen.js
+++ components/WelcomeScreen.js
@@ -1,34 +1,46 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, StyleSheet } from "react-native";
 
 export default function WelcomeScreen() {
   return (
-    <>
-      <View style={{ flex: 1, backgroundColor: "#495E57" }}>
-        <Text
-          style={{
-            marginTop: 30,
-            fontSize: 30,
-            color: "#F4CE14",
-            textAlign: "center",
-            fontWeight: "bold",
-          }}
-        >
-          Welcome to Little Lemon
-        </Text>
-        <Text
-          style={{
-            padding: 40,
-            fontSize: 20,
-            color: "#F4CE14",
-            textAlign: "center",
-          }}
-        >
+    <View style={welcomeStyles.container}>
+      <ScrollView
+        indicatorStyle={"white"}
+        style={welcomeStyles.scrollContainer}
+      >
+        <Text style={welcomeStyles.titleText}>Welcome to Little Lemon</Text>
+        <Text style={welcomeStyles.itemText}>
           Little Lemon is a charming neighborhood bistro that serves simple food
           and classic cocktails in a lively but casual environment. We would
-          love to hear more about your experience with us !
+          love to hear more about your experience with us ! {"\n"} {"\n"}
+          Come and take a bite! You will surely forget your name!
         </Text>
-      </View>
-    </>
+      </ScrollView>
+    </View>
   );
 }
+
+const welcomeStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#495E57",
+  },
+  scrollContainer: {
+    paddingHorizontal: 40,
+    paddingVertical: 40,
+  },
+  titleText: {
+    padding: 40,
+    fontSize: 50,
+    color: "#F4CE14",
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  itemText: {
+    fontSize: 30,
+    padding: 40,
+    marginVertical: 8,
+    color: "#F4CE14",
+    textAlign: "center",
+  },
+});
