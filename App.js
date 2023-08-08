493039e74d989a4ca6bdb52ca4eede8a47e77d40
Carol Villadelgado <carolvilladelgado@gmail.com>
2023-08-08 17:48:14 +0800

    Update App

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { View } from "react-native";
 
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
+import WelcomeScreen from "./components/WelcomeScreen";
+
 export default function App() {
   return (
     <>
@@ -13,6 +15,7 @@ export default function App() {
         }}
       >
         <LittleLemonHeader />
+        <WelcomeScreen />
       </View>
       <View style={{ backgroundColor: "#495E57" }}>
         <LittleLemonFooter />
