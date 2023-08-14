99c4ff309b99a9c2f4228710d5cd0faa3b2f474b
Carol Villadelgado <carolvilladelgado@gmail.com>
2023-08-14 22:49:48 +0800

    Add loginscreen

---
+++ components/LoginScreen.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+
+export default function LoginScreen() {
+  const [email, onChangeEmail] = useState("");
+  const [username, onChangeUsername] = useState("");
+  const [phoneNumber, onChangePhoneNumber] = useState("");
+  const [password, onChangePassword] = useState("");
+
+  return (
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView style={styles.container} keyboardDismissMode="on-drag">
+        <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+        <Text style={styles.regularText}>Login to continue </Text>
+        <TextInput
+          style={styles.inputBox}
+          value={email}
+          onChangeText={onChangeEmail}
+          placeholder={"Email"}
+          keyboardType={"email-address"}
+        />
+        <TextInput
+          // secureTextEntry={true} //for passwords
+          style={styles.inputBox}
+          value={username}
+          onChangeText={onChangeUsername}
+          placeholder={"Username"}
+        />
+
+        <TextInput
+          style={styles.inputBox}
+          value={phoneNumber}
+          onChangeText={onChangePhoneNumber}
+          placeholder={"Phone Number"}
+          keyboardType={"phone-pad"}
+        />
+        <TextInput
+          secureTextEntry={true}
+          style={styles.inputBox}
+          value={password}
+          onChangeText={onChangePassword}
+          placeholder={"Password"}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerText: {
+    padding: 40,
+    fontSize: 30,
+    color: "#EDEFEE",
+    textAlign: "center",
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEFEE",
+    textAlign: "center",
+  },
+  inputBox: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 16,
+    backgroundColor: "#EDEFEE",
+  },
+});
