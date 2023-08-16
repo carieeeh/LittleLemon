f93f5683a713d74129c6578164e916c2c7d1465e
Carol Villadelgado <carolvilladelgado@gmail.com>
2023-08-16 16:55:59 +0800

    Update Feedbackform

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,55 +1,88 @@
 import React, { useState } from "react";
-import { ScrollView, StyleSheet, Text, TextInput } from "react-native";
+import {
+  ScrollView,
+  Text,
+  StyleSheet,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
-const FeedbackForm = () => {
-  // declare the variables
+export default function FeedbackForm() {
   const [firstName, onChangeFirstName] = useState("");
   const [lastName, onChangeLastName] = useState("");
-  const [message, onChangeMessage] = useState("");
   const [phoneNumber, onChangePhoneNumber] = useState("");
+  const [message, onChangeMessage] = useState("");
 
   return (
-    <ScrollView style={styles.container}>
-      <Text style={styles.headingSection}>
-        How was your visit to Little Lemon?
-      </Text>
-
-      <Text style={styles.infoSection}>
-        ittle Lemon is a charming neighborhood bistro that serves simple food
-        and classic cocktails in a lively but casual environment. We would love
-        to hear your experience with us!
-      </Text>
-      <TextInput
-        style={styles.input}
-        value={firstName}
-        onChangeText={onChangeFirstName}
-      />
-      <TextInput
-        style={styles.input}
-        value={lastName}
-        onChangeText={onChangeLastName}
-      />
-      <TextInput
-        style={styles.messageInput}
-        value={message}
-        onChangeText={onChangeMessage}
-      />
-    </ScrollView>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView style={styles.container} keyboardDismissMode="on-drag">
+        <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+        <Text style={styles.regularText}>
+          Little Lemon is a charming neighborhood bistro that serves simple food
+          and classic cocktails in a lively but casual environment. We would
+          love to hear more about your experience with us!
+        </Text>
+        <TextInput
+          style={styles.inputBox}
+          value={firstName}
+          onChangeText={onChangeFirstName}
+          placeholder={"First Name"}
+        />
+        <TextInput
+          // secureTextEntry={true} //for passwords
+          style={styles.inputBox}
+          value={lastName}
+          onChangeText={onChangeLastName}
+          placeholder={"Last Name"}
+        />
+        <TextInput
+          style={styles.messageInput}
+          value={message}
+          onChangeText={onChangeMessage}
+          placeholder={"Please leave a feedback"}
+          multiline={true} //for multiple lines
+          maxLength={250}
+        />
+        <TextInput
+          style={styles.inputBox}
+          value={phoneNumber}
+          onChangeText={onChangePhoneNumber}
+          placeholder={"Phone Number"}
+          keyboardType={"phone-pad"}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  input: {
+  headerText: {
+    padding: 40,
+    fontSize: 30,
+    color: "#EDEFEE",
+    textAlign: "center",
+  },
+  regularText: {
+    fontSize: 24,
+    padding: 20,
+    marginVertical: 8,
+    color: "#EDEFEE",
+    textAlign: "center",
+  },
+  inputBox: {
     height: 40,
     margin: 12,
     borderWidth: 1,
     padding: 10,
     fontSize: 16,
-    borderColor: "EDEFEE",
-    backgroundColor: "#F4CE14",
+    backgroundColor: "#EDEFEE",
   },
   messageInput: {
     height: 100,
@@ -57,24 +90,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     fontSize: 16,
-    backgroundColor: "#F4CE14",
-  },
-  infoSection: {
-    fontSize: 24,
-    padding: 20,
-    marginVertical: 8,
-    color: "#EDEFEE",
-    textAlign: "center",
-    backgroundColor: "#495E57",
-  },
-  headingSection: {
-    fontSize: 28,
-    padding: 20,
-    marginVertical: 8,
-    color: "#EDEFEE",
-    textAlign: "center",
-    backgroundColor: "#495E57",
+    backgroundColor: "#EDEFEE",
   },
 });
-
-export default FeedbackForm;
