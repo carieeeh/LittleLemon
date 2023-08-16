6715d001c65481b58db40ff40fc80995d6816090
Carol Villadelgado <carolvilladelgado@gmail.com>
2023-08-16 16:56:36 +0800

    Update App

--- App.js
+++ App.js
@@ -6,16 +6,17 @@ import LittleLemonFooter from "./components/LittleLemonFooter";
 import WelcomeScreen from "./components/WelcomeScreen";
 import MenuItems from "./components/MenuItems";
 import LoginScreen from "./components/LoginScreen";
+import FeedbackForm from "./components/FeedbackForm";
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
         <LittleLemonHeader />
-
-        {/* <WelcomeScreen /> */}
+        {/* <FeedbackForm /> */}
+        <WelcomeScreen />
         {/* <MenuItems /> */}
-        <LoginScreen />
+        {/* <LoginScreen /> */}
       </View>
       <View style={{ backgroundColor: "#495E57" }}>
         <LittleLemonFooter />
