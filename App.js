b16b9c732cf4be7c9ec0c82c43150b4a1b7876ca
Carie Carol <carolvilladelgado@gmail.com>
2023-08-17 16:50:23 +0800

    Update App

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import * as React from "react";
-import { View, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  useColorScheme,
+  useWindowDimensions,
+} from "react-native";
+import { useDeviceOrientation } from "@react-native-community/hooks";
 
 import LittleLemonHeader from "./components/LittleLemonHeader";
 import LittleLemonFooter from "./components/LittleLemonFooter";
@@ -9,11 +16,31 @@ import LoginScreen from "./components/LoginScreen";
 import FeedbackForm from "./components/FeedbackForm";
 
 export default function App() {
+  //HOOKS SAMPLES
+  // const colorScheme = useColorScheme();
+  // const window = useWindowDimensions();
+  // const orientation = useDeviceOrientation();
+  // console.log("is orientation portrait: ", orientation.portrait);
+  // console.log("is orientation landscape: ", orientation.landscape);
+
   return (
     <>
-      <View style={styles.container}>
+      <View
+        style={[
+          styles.container,
+          // colorScheme === "light"
+          //   ? { backgroundColor: "#fff" }
+          //   : { backgroundColor: "#333333" },
+        ]}
+      >
         <LittleLemonHeader />
         {/* <FeedbackForm /> */}
+
+        {/* <Text>Window Dimensions</Text>
+        <Text>Height: {window.height}</Text>
+        <Text>Width: {window.width}</Text>
+        <Text>Font scale: {window.fontScale}</Text> */}
+
         <WelcomeScreen />
         {/* <MenuItems /> */}
         {/* <LoginScreen /> */}
